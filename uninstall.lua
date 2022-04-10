e59cbef599a7e5cc1df0c37240912cfaaa6933df
FADRI-594 <69135068+FADRI-594@users.noreply.github.com>
2022-04-11 03:52:23 +0800

    Add files via upload

---
+++ uninstall.lua
@@ -0,0 +1,4 @@
+os.execute("rm -fr lib")    -- Удаление всей библиотеки
+os.execute("rm -fr main.lua")   -- Удаление исполняющего файла
+os.execute("rm -fr config.txt") -- Удаление конфига
+os.execute("rm -fr uninstall.lua")  -- Удаление файла удаления
