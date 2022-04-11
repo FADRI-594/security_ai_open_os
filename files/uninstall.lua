d2ed8f5f62e507fcf92ec0ed3179268c51f22da1
FADRI-594 <fadri.594.gold@gmail.com>
2022-04-11 23:28:48 +0800

    none

--- files/uninstall.lua
+++ files/uninstall.lua
@@ -8,18 +8,18 @@ term.clear()
 -- Первый элемент - название файла, второй - директория для сохранения файла.
 local applications = {
     -- Основые библиотеки
-    { "lib", "/home/" },  -- Библиотека
+    "/home/lib",  -- Библиотека
     -- Основные файлы
-    { "main.lua", "/home/" },  -- Исполняющий файл
-    { "config.txt", "/home/" },  -- Конфиг файл
-    { "uninstall.lua", "/home/" }  -- Файл удаления
+    "/home/main.lua",  -- Исполняющий файл
+    "/home/config.txt",  -- Конфиг файл
+    "/home/uninstall.lua"  -- Файл удаления
 }
 
 
 
 -- Удаление файлов
 for i = 1, #applications do
-    print("Удаляется: " .. applications[i][2] .. applications[i][1])
-    shell.execute("rm -fr " .. applications[i][2] .. applications[i][1])
+    print("Удаляется: " .. applications[i])
+    shell.execute("rm -fr " .. applications[i])
     os.sleep(0.3)
 end
--- installer.lua
+++ installer.lua
@@ -8,13 +8,13 @@ term.clear()
 -- Первый элемент - название файла, второй - ссылка на файл, третий - директория для сохранения файла.
 local applications = {
   -- Основные файлы
-  { "main.lua", "/home/", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/main.lua" },  -- Исполняющий файл
-  { "config.txt", "/home/", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/config.txt" },  -- Конфиг файл
-  { "uninstall.lua", "/home/", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/uninstall.lua" },  -- Файл удаления
+  { "/home/main.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/main.lua" },  -- Исполняющий файл
+  { "/home/config.txt", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/config.txt" },  -- Конфиг файл
+  { "/home/uninstall.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/uninstall.lua" },  -- Файл удаления
   -- Основые библиотеки
-  { "data.lua", "/home/lib/", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/data.lua" },  -- Библиотека данных
-  { "cmd.lua", "/home/lib/", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/cmd.lua" },  -- Библиотека данных
-  { "access.lua", "/home/lib/", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/access.lua" }  -- Библиотека данных
+  { "/home/lib/data.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/data.lua" },  -- Библиотека данных
+  { "/home/lib/cmd.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/cmd.lua" },  -- Библиотека данных
+  { "/home/lib/access.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/access.lua" }  -- Библиотека данных
 }
 
 -- Создание папок
@@ -22,9 +22,9 @@ os.execute("mkdir lib") -- Создание папки Библиотека
 
 -- Загрузка файлов
 for i = 1, #applications do
-  print("Устанавливается: " .. applications[i][2] .. applications[i][1])
-  fs.makeDirectory(fs.path(applications[i][2] .. applications[i][1]) or "")		
-  shell.execute("wget " .. applications[i][3] .. " " .. applications[i][2] .. applications[i][1] .. " -fQ")
+  print("Устанавливается " .. applications[i][1])
+  fs.makeDirectory(fs.path(applications[i][1]) or "")			
+  shell.execute("wget " .. applications[i][2] .. " " .. applications[i][1] .. " -fQ")
   os.sleep(0.3)
 end
 
