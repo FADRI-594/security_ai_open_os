efc7257fdbe1e0e3cb480146f0c62375717664db
FADRI-594 <fadri.594.gold@gmail.com>
2022-04-12 22:33:39 +0800

    Update access.lua

--- files/lib/access.lua
+++ files/lib/access.lua
@@ -5,32 +5,28 @@ local say = component.chat_box.say
 local data = require("data")  -- Библиотека данных
 
 
--- Поиск ника в admins
-function access.FindNick(nick, admins)
+-- Объединение никнеймов
+function access.NA_in_Line(admins)
+  local NewAdmins = ""  -- Список никнеймов админов
 
-  local st
 
-  if(#admins == 1 and admins[1] == "пусто") then
-    st = "пусто"
-    print("st: " .. st)
+  if(admins[1] == "пусто") then -- Если admins пуста (в этом случае всегда admins[1] = "пусто")
+    NewAdmins = "пусто"
   else
 
-    -- Поиск ника в admins
+    -- Перебор списка админов
     for i = 1, #admins do
-      st = "не найдено"
-      print("st: " .. st)
-      if(admins[i] == nick) then  -- Если ник найден в admins
-        st = "найдено"
-        print("st: " .. st)
-        break -- Перестать искать
+      if(i == 1) then -- Если это первая запись в admins
+        NewAdmins = admins[i]
+      else
+        NewAdmins = NewAdmins .. "," .. admins[i]
       end
     end
 
   end
 
-  
 
-  return st
+  return NewAdmins
 end
 
 
@@ -38,23 +34,44 @@ end
 
 
 -- Получение прав администратора
-function access.Get(nick, admins)
+function access.Get(nick, msg, admins)
 
-  local st =  access.FindNick(nick, admins)
+  local NewAdmins = access.NA_in_Line(admins)
+  print("\nNewAdmins: " .. NewAdmins .. "\n")
 
 
-  if(st == "пусто") then  -- Если таблица в admins нет ников
-    say(nick .. ", вам присвоены права!")
-    admins = data.Update(nick, "admins", "add") -- Вызов функции записи в конфиг
-  else
-    if(st == "не найдено") then -- Если в admins нет ника игрока
-      say(nick .. ", у вас нет прав на использование этой команды!")
-    elseif(st == "найдено") then  -- Если в admins есть ник
-      say(nick .. ", у вас уже есть права!")
-    end
+  print("Admins:\n")
+  for i = 1, #admins do
+    print(admins[i])
   end
 
 
+
+  
+  if(NewAdmins == "пусто") then -- Если строка ников пуста
+    say(nick .. ", вам присвоены права!")
+    admins = data.Update(nick, "admins", "add") -- Вызов функции записи в конфиг
+  else  -- Если есть хотя бы 1 ник
+    
+    if(NewAdmins:find(nick) ~= nil) then  -- Если в admins нет ника игрока
+      say(nick .. ", у вас нет прав на использование этой команды!")
+    else  -- Если в admins есть ник
+      
+
+      local start_pos, end_pos = NewAdmins:find(nick)
+      if((start_pos == 1 or NewAdmins:sub(start_pos-1) == ",") and (end_pos == NewAdmins:len() or NewAdmins:sub(end_pos+1) == ",")) then
+        
+        say(nick .. ", у вас уже есть права!")
+
+      else
+        say(nick .. ", у вас нет прав на использование этой команды!")
+      end
+
+
+    end
+
+  end
+  
 
   return admins
 end
