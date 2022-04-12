167098d5eaa44f2904f8a764d47a121069fcb70f
FADRI-594 <fadri.594.gold@gmail.com>
2022-04-12 21:35:57 +0800

    Update access.lua

--- files/lib/access.lua
+++ files/lib/access.lua
@@ -5,51 +5,29 @@ local say = component.chat_box.say
 local data = require("data")  -- Библиотека данных
 
 
--- Поиск ника игрока в таблице админов
-function access.FindNick(nick, admins)
-  
-  local found = false -- Ник найден (изначально false)
-  local empty = true -- Таблица пуста
+-- Поиск ника в admins
+function FindNick(nick, admins)
 
+  local st
 
-  -- Проверка пустая таблица или нет
-  if(#admins == 1) then
-    if(admins[1] == "пусто") then
-      empty = true
-      print("Таблица пуста! = кол-во эл-ов таблицы: " .. #admins .. ", первый элемент: " .. admins[1])
-    else
-      empty = false
-      print("Таблица не пуста! = кол-во эл-ов таблицы: " .. #admins .. ", первый элемент: " .. admins[1])
-    end
+  if(#admins == 1 and admins[1] == "пусто") then
+    st = "пусто"
   else
-    empty = false
-    print("Таблица не пуста! = кол-во эл-ов таблицы: " .. #admins .. ", первый элемент: " .. admins[1])
-  end
 
-
-  -- Если таблица не пуста
-  if(empty == false) then
-    
-    -- Поиск ника среди admins
+    -- Поиск ника в admins
     for i = 1, #admins do
-      if(admins[i] == nick) then  -- Если ник найден в базе
-        
-        found = true  -- Игрок найден!
-
-        print("Ник найден в таблице! = место в таблице: " .. i .. ", ник в базе: " .. admins[i])
-  
-        break -- Прекратить поиск игрока
+      st = "не найдено"
+      if(admins[i] == nick) then  -- Если ник найден в admins
+        st = "найдено"
+        break -- Перестать искать
       end
     end
 
   end
 
-  if(found == false) then
-    print("Ник не найден в таблице! Ник: " .. nick)
-  end
-  
 
-  return empty, found
+
+  return st
 end
 
 
@@ -59,25 +37,18 @@ end
 -- Получение прав администратора
 function access.Get(nick, admins)
 
+  local st =  FindNick(nick, admins)
 
-  local empty, found = access.FindNick(nick, admins)
 
-
-  -- Если таблица пуста
-  if(empty) then
-    
+  if(st == "пусто") then  -- Если таблица в admins нет ников
     say(nick .. ", вам присвоены права!")
     admins = data.Update(nick, "admins", "add") -- Вызов функции записи в конфиг
-
-  -- Если таблица не пуста
   else
-
-    if found then -- Если ник найден
-      say(nick .. ", у вас уже есть права!")
-    else  -- Если ник не найден
+    if(st == "не найдено") then -- Если в admins нет ника игрока
       say(nick .. ", у вас нет прав на использование этой команды!")
+    elseif(st == "найдено") then  -- Если в admins есть ник
+      say(nick .. ", у вас уже есть права!")
     end
-
   end
 
 
