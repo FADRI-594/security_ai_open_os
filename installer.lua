ce99a2b18103606869ee40985b66bfc15080e242
FADRI-594 <fadri.594.gold@gmail.com>
2022-04-13 22:54:48 +0800

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -14,15 +14,14 @@ local applications = {
   -- Основые библиотеки
   { "/home/lib/data.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/data.lua" },  -- Библиотека данных
   { "/home/lib/cmd.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/cmd.lua" },  -- Библиотека данных
-  { "/home/lib/access.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/access.lua" }  -- Библиотека данных
+  { "/home/lib/access.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/access.lua" },  -- Библиотека данных
+  { "/home/lib/detected.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/detect.lua" },  -- Библиотека данных
+  { "/home/lib/cipher.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/cipher.lua" }  -- Библиотека данных
 }
-
--- Создание папок
-os.execute("mkdir lib") -- Создание папки Библиотека
 
 -- Загрузка файлов
 for i = 1, #applications do
-  print("Устанавливается " .. applications[i][1])
+  print("Устанавливается: " .. applications[i][1])
   fs.makeDirectory(fs.path(applications[i][1]) or "")			
   shell.execute("wget " .. applications[i][2] .. " " .. applications[i][1] .. " -fQ")
   os.sleep(0.3)
@@ -33,7 +32,7 @@ end
 -- Удаление файла установки
 os.execute("rm -fr installer.lua")
 
-print("Готово")
+print("Готово!\n")
 
 
 -- Запуск ИИ сразу
