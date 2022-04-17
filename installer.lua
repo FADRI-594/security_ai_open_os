fa47084658fe258ee902f6dcfbd7273756439d78
FADRI-594 <fadri.594.gold@gmail.com>
2022-04-17 09:33:40 +0800

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -9,7 +9,7 @@ term.clear()
 local applications = {
   -- Основные файлы
   { "/home/main.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/main.lua" },  -- Исполняющий файл
-  { "/home/thread.lua", "" },  -- Ядра
+  { "/home/thread.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/thread.lua" },  -- Ядра
   { "/home/config.txt", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/config.txt" },  -- Конфиг файл
   { "/home/uninstall.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/uninstall.lua" },  -- Файл удаления
   -- Основые библиотеки
@@ -19,10 +19,10 @@ local applications = {
   { "/home/lib/detect.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/detect.lua" },  -- Библиотека данных
   { "/home/lib/cipher.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/lib/cipher.lua" },  -- Библиотека данных
   -- Библиотеки вывода
-  { "/home/displaying/images.lua", "" },  -- Библиотека изображений
-  { "/home/displaying/paints.lua", "" },  -- Библиотека рисования на экране
-  { "/home/displaying/animate.lua", "" },  -- Библиотека анимации
-  { "/home/displaying/output.lua", "" }  -- Библиотека вывода на экран
+  { "/home/displaying/images.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/displaying/images.lua" },  -- Библиотека изображений
+  { "/home/displaying/paint.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/displaying/paint.lua" },  -- Библиотека рисования на экране
+  { "/home/displaying/animate.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/displaying/animate.lua" },  -- Библиотека анимации
+  { "/home/displaying/output.lua", "https://raw.githubusercontent.com/FADRI-594/security_ai_open_os/security_ai_v.1.0/files/displaying/output.lua" }  -- Библиотека вывода на экран
 }
 
 -- Загрузка файлов
